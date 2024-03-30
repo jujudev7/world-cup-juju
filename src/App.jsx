a167986d03b5949d9d694b897914a6b5cf1003cf
Julien <penna.julien@gmail.com>
2024-03-30 10:14:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return(
+    <div>
+      <h1>Coupe du monde de Football</h1>
+      <p>Bienvenue sur le site qui répertorie toutes les infos de la plus grande compétition du sport le plus populaire de la planète !</p>
+    </div>
+  )
+}
+
+export default App;
